27de9810f0b772edbaac6bd666aeb1052ac2cf6e
Jamie Klassen <cklassen@pivotal.io>
2019-01-21 09:06:48 -0500

    Refactor/rename tests

--- tests/Example.elm
+++ tests/Example.elm
@@ -19,43 +19,41 @@ find id ((Main.Node iden _ _ _ _) as node) =
 
 suite : Test
 suite =
-    describe "view"
+    describe "view" <|
+        let
+            topBar : () -> Maybe Main.Node
+            topBar _ =
+                Main.page |> find Main.TopBar
+        in
         [ test "resourcePage has dark background top bar" <|
-            \_ ->
-                Main.page
-                    |> find Main.TopBar
-                    |> Maybe.map Main.styles
-                    |> Maybe.andThen .backgroundColor
-                    |> Expect.equal (Just Main.AlmostBlack)
-        , test "top bar contains search input" <|
-            \_ ->
-                Main.page
-                    |> find Main.TopBar
-                    |> Maybe.andThen (find Main.SearchInput)
-                    |> (/=) Nothing
-                    |> Expect.true "could not find search input"
-        , test "top bar has transparent background" <|
-            \_ ->
-                Main.page
-                    |> find Main.TopBar
-                    |> Maybe.andThen (find Main.SearchInput)
-                    |> Maybe.map Main.styles
-                    |> Maybe.andThen .backgroundColor
-                    |> Expect.equal (Just Main.Transparent)
-        , test "top bar has monospace font" <|
-            \_ ->
-                Main.page
-                    |> find Main.TopBar
-                    |> Maybe.andThen (find Main.SearchInput)
-                    |> Maybe.map Main.styles
-                    |> Maybe.andThen .fontFamily
-                    |> Expect.equal (Just Main.Inconsolata)
-        , test "top bar has light grey border" <|
-            \_ ->
-                Main.page
-                    |> find Main.TopBar
-                    |> Maybe.andThen (find Main.SearchInput)
-                    |> Maybe.map Main.styles
-                    |> Maybe.andThen .border
-                    |> Expect.equal (Just Main.LighterGrey)
+            topBar
+                >> Maybe.map Main.styles
+                >> Maybe.andThen .backgroundColor
+                >> Expect.equal (Just Main.AlmostBlack)
+        , describe "search input" <|
+            let
+                searchInput : () -> Maybe Main.Node
+                searchInput =
+                    topBar >> Maybe.andThen (find Main.SearchInput)
+            in
+            [ test "top bar contains search input" <|
+                searchInput
+                    >> (/=) Nothing
+                    >> Expect.true "could not find search input"
+            , test "search bar has transparent background" <|
+                searchInput
+                    >> Maybe.map Main.styles
+                    >> Maybe.andThen .backgroundColor
+                    >> Expect.equal (Just Main.Transparent)
+            , test "search bar has monospace font" <|
+                searchInput
+                    >> Maybe.map Main.styles
+                    >> Maybe.andThen .fontFamily
+                    >> Expect.equal (Just Main.Inconsolata)
+            , test "search bar has light grey border" <|
+                searchInput
+                    >> Maybe.map Main.styles
+                    >> Maybe.andThen .border
+                    >> Expect.equal (Just Main.LighterGrey)
+            ]
         ]
